cab932115ade2d100d41180648151b4d3bb68ce2
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-13 23:48:15 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by KEN NAKAI on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
